0dedbf8c64ce5adcee3329bf015d28a179aa9205
alijan1978 <mohammadalinaim32@gmail.com>
2023-10-26 09:00:38 -0700

    The engine specification has been added

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,18 @@ public class Main {
         audi.color="Black";
         audi.model="A3";
         audi.power="5000 cc";
+
+
+        Engine engine=new Engine();
+        engine.cylinders="8";
+        engine.size="450 cc";
+        engine.weight="6500kg";
+        System.out.println(engine.cylinders);
+        System.out.println(engine.size);
+        System.out.println(engine.weight);
+
+
+
     }
 }
 // video Class 5 Merging  min 45
